a3ff349104a45ad46e129385d334e3f93c608cf0
Rommel Soriano <53027073+rolmen26@users.noreply.github.com>
2024-05-07 10:59:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    'base': '/react-redux-example/',
     port: 3000
   },
   plugins: [react()],
